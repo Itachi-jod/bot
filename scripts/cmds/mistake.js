3134fd5d22db61f85d8148a50edb9289eb742953
Itachi <itachijodx72@gmail.com>
2025-08-18 15:58:43 +0545

    Update mistake.js

--- scripts/cmds/mistake.js
+++ scripts/cmds/mistake.js
@@ -1,52 +1,58 @@
-const axios = require('axios');
-const jimp = require("jimp");
+const axios = require("axios");
 const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   config: {
     name: "mistake",
-    aliases: ["mistake"],
-    version: "1.0",
-    author: "Jester McGregor",//don't change credit otherwise I'm gonna fuck your mom
-    countDown: 10,//to avoid spam i did 10sec you can change
+    aliases: ["msk"],
+    version: "1.1",
+    author: "MinatoCodes",
+    countDown: 5,
     role: 0,
-    shortdescription: "a mistake",
-    longDescription: "a small mistake (mention someone)",//suuuuuuuuuuuuuuuuuuuuiiiiii 
-    category: "ENTERTAINMENT",
-    guide: ""
+    shortDescription: "Mistake profile image",
+    longDescription: "Generate a Mistake effect profile image",
+    category: "image",
+    guide: {
+      en: "{pn} @tag"
+    }
   },
 
-  onStart: async function ({ message, event, args }) {
-    const mention = Object.keys(event.mentions);
-    if (mention.length == 0) {
-      message.reply("You must select tag a person❗");
-      return;
+  langs: {
+    en: {
+      noTag: "You must tag the person you want to get profile picture of"
     }
+  },
 
-    let one;
-    if (mention.length == 1) {
-      one = mention[0];
+  onStart: async function ({ event, message, usersData }) {
+    let avt;
+    const uid1 = event.senderID;
+    const uid2 = Object.keys(event.mentions)[0];
+
+    if (event.type === "message_reply") {
+      avt = await usersData.getAvatarUrl(event.messageReply.senderID);
     } else {
-      one = mention[0];
+      if (!uid2) {
+        avt = await usersData.getAvatarUrl(uid1);
+      } else {
+        avt = await usersData.getAvatarUrl(uid2);
+      }
     }
 
-    try {
-      const imagePath = await bal(one);
-      await message.reply({
-        body: "「The Biggest Mistake」",
-        attachment: fs.createReadStream(imagePath)
-      });
-    } catch (error) {
-      console.error("Error while running command:", error);
-      await message.reply("an error occurred");
-    }
+    // API call with avatar as query
+    const apiUrl = `https://mistake-api.vercel.app/api/mistake?avatar=${encodeURIComponent(avt)}`;
+
+    // Download image to temp folder
+    const imagePath = path.join(__dirname, "cache", `mistake_${Date.now()}.jpg`);
+    const response = await axios.get(apiUrl, { responseType: "arraybuffer" });
+    fs.writeFileSync(imagePath, response.data);
+
+    // Send message with fixed body + image
+    message.reply({
+      body: "「The Biggest Mistake」",
+      attachment: fs.createReadStream(imagePath)
+    }, () => {
+      fs.unlinkSync(imagePath); // delete after sending
+    });
   }
 };
-async function bal(one) {
-  const avatarone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
-  const image = await jimp.read("https://i.postimg.cc/2ST7x1Dw/received-6010166635719509.jpg");
-  image.resize(512, 512).composite(avatarone.resize(220, 203), 145, 305);
-  const imagePath = "mistake.png";
-  await image.writeAsync(imagePath);
-  return imagePath;
-    }
